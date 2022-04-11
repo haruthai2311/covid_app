f9af8afd1fb05e5360e670505ac1b6d7ef73cd03
PangSuwatcharaporn <suwatcharaporn.m@ku.th>
2022-04-11 20:14:22 +0700

    bottom

--- lib/screens/bottom_nav_screen.dart
+++ lib/screens/bottom_nav_screen.dart
@@ -1,8 +1,8 @@
-
 import 'package:covid_app/screens/emergency.dart';
 import 'package:covid_app/screens/home_screen.dart';
 import 'package:covid_app/screens/info_screen.dart';
 import 'package:covid_app/screens/state_screen.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavScreen extends StatefulWidget {
@@ -11,54 +11,46 @@ class BottomNavScreen extends StatefulWidget {
 }
 
 class _BottomNavScreenState extends State<BottomNavScreen> {
+  int _currentIndex = 0;
+  final navigationKey = GlobalKey<CurvedNavigationBarState>();
+
   final List _screens = [
     StateScreen(),
     HomeScreen(),
     emergencyScreen(),
     InfoScreen(),
-    //Scaffold(),
-    //Scaffold(),
-    //InfoScreen(),
   ];
-  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        selectedItemColor: Color.fromARGB(255, 255, 255, 255),
-        unselectedItemColor: Colors.grey,
-        elevation: 0.0,
-        items: [Icons.home, Icons.event_note_rounded, Icons.call, Icons.info]
-            .asMap()
-            .map((key, value) => MapEntry(
-                  key,
-                  BottomNavigationBarItem(
-                    label: '',
-                    icon: Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 6.0,
-                        horizontal: 16.0,
-                      ),
-                      decoration: BoxDecoration(
-                        color: _currentIndex == key
-                            ? Color.fromARGB(255, 58, 2, 63)
-                            : Colors.transparent,
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      child: Icon(value),
-                    ),
-                  ),
-                ))
-            .values
-            .toList(),
+    return Container(
+      child: SafeArea(
+        top: false,
+        child: Scaffold(
+          extendBody: true,
+          body: _screens[_currentIndex],
+          bottomNavigationBar: Theme(
+            data: Theme.of(context)
+                .copyWith(iconTheme: IconThemeData(color: Colors.white)),
+            child: CurvedNavigationBar(
+              key: navigationKey,
+              color: Colors.black45,
+              backgroundColor: Colors.transparent,
+              buttonBackgroundColor: Color.fromARGB(255, 111, 4, 138),
+              height: 50,               /// 60.0
+              animationCurve: Curves.easeInOut,
+              animationDuration: Duration(milliseconds: 400),
+              index: _currentIndex,
+              onTap: (index) => setState(() => _currentIndex = index),
+              items: <Widget>[
+                Icon(Icons.home, size: 30),
+                Icon(Icons.event_note_rounded, size: 30),
+                Icon(Icons.call, size: 30),
+                Icon(Icons.info, size: 30),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
